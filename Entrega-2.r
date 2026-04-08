174d6eec651a59e8c18ec827111abcc842a69e27
Cristóbal Herrera Reyes <cristobalherrerareyes@MacBook-Air-de-Cristobal.local>
2026-04-08 10:55:07 -0500

    Instalación Paquetes

---
+++ Entrega-2.r
@@ -0,0 +1,18 @@
+# CASO: HOLLYWOOD RULES
+Ivan Mallorga, Cristóbal Herrera
+# Analítica de Negocios – Pontificia Universidad Javeriana
+# =============================================================
+if (!require(readxl))   install.packages("readxl")
+if (!require(ggplot2))  install.packages("ggplot2")
+if (!require(dplyr))    install.packages("dplyr")
+if (!require(car))      install.packages("car")
+if (!require(scales))   install.packages("scales")
+if (!require(gridExtra))install.packages("gridExtra")
+ 
+library(readxl)
+library(ggplot2)
+library(dplyr)
+library(car)
+library(scales)
+library(gridExtra)
+
